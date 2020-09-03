b300e389e6a807a4cf96a0ab31585208ba5b3342
jungho5045 <jungho5045@naver.com>
2020-09-04 00:48:01 +0900

    [refactor] "Repository Test"

---
+++ src/main/java/com/fastcampus/javaallinone/project3/mycontact/domain/Person.java
@@ -0,0 +1,82 @@
+package com.fastcampus.javaallinone.project3.mycontact.domain;
+
+import com.fastcampus.javaallinone.project3.mycontact.controller.dto.PersonDto;
+import com.fastcampus.javaallinone.project3.mycontact.domain.dto.Birthday;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.Where;
+import org.springframework.util.StringUtils;
+
+import javax.persistence.*;
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Data
+@Where(clause = "deleted = false")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    @NonNull
+    @NotEmpty
+    @Column(nullable = false)
+    protected String name;
+
+    protected String hobby;
+
+    protected String address;
+
+    @Valid
+    @Embedded
+    protected Birthday birthday;
+
+    protected String job;
+
+    protected String phoneNumber;
+
+    @ColumnDefault("0")
+    private boolean deleted;
+
+    public void set(PersonDto personDto){
+
+        if(!StringUtils.isEmpty(personDto.getHobby())){
+            this.setHobby(personDto.getHobby());
+        }
+
+        if(!StringUtils.isEmpty(personDto.getAddress())){
+            this.setAddress(personDto.getAddress());
+        }
+
+        if(!StringUtils.isEmpty(personDto.getJob())){
+            this.setJob(personDto.getJob());
+        }
+
+        if(!StringUtils.isEmpty(personDto.getPhoneNumber())){
+            this.setPhoneNumber(personDto.getPhoneNumber());
+        }
+
+        if(personDto.getBirthday() != null){
+            this.setBirthday(Birthday.of(personDto.getBirthday()));
+        }
+
+    }
+
+    public Integer getAge(){
+        if(this.birthday != null){
+            return LocalDate.now().getYear() - this.birthday.getYearOfBirthday() + 1;
+        } else {
+            return null;
+        }
+    }
+
+    public boolean isBirthdayToday(){
+        return LocalDate.now().equals(LocalDate.of(this.birthday.getYearOfBirthday(), this.birthday.getMonthOfBirthday(), this.birthday.getDayOfBirthday()));
+    }
+}
