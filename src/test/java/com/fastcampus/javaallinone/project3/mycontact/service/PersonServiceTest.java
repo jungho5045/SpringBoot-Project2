6b22e428efb8d18428f6e9fd7c3583ad0d37acbf
jungho5045 <jungho5045@naver.com>
2020-09-09 15:58:09 +0900

    [refactor] "PersonService의 getPerson(), put() 메소드에 Test를 작성한다."

--- src/test/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonServiceTest.java
+++ src/test/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonServiceTest.java
@@ -1,5 +1,6 @@
 package com.fastcampus.javaallinone.project3.mycontact.service;
 
+import com.fastcampus.javaallinone.project3.mycontact.controller.dto.PersonDto;
 import com.fastcampus.javaallinone.project3.mycontact.domain.Person;
 import com.fastcampus.javaallinone.project3.mycontact.repository.PersonRepository;
 import org.assertj.core.util.Lists;
@@ -9,10 +10,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class PersonServiceTest {
@@ -32,6 +36,35 @@ class PersonServiceTest {
 
         assertThat(result.size()).isEqualTo(1);
         assertThat(result.get(0).getName()).isEqualTo("martin");
+    }
+
+    @Test
+    void getPerson() {
+        when(personRepository.findById(1L))
+                .thenReturn(Optional.of(new Person("martin")));
+
+        Person person = personService.getPerson(1L);
+
+        assertThat(person.getName()).isEqualTo("martin");
+    }
+
+    @Test
+    void getPersonIfnotFound() {
+        when(personRepository.findById(1L))
+                .thenReturn(Optional.empty());
+
+        Person person = personService.getPerson(1L);
+
+        assertThat(person).isNull();
+    }
+
+    @Test
+    void put() {
+        PersonDto dto = PersonDto.of("martin", "programing", "성남", LocalDate.now(), "programer", "010-1111-2222");
+
+        personService.put(dto);
+
+        verify(personRepository, times(1)).save(any(Person.class));
     }
 
 }
