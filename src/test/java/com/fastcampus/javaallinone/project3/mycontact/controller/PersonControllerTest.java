b6262e4b6a44a83ba2b6c211a7bc4d2caec71519
jungho5045 <jungho5045@naver.com>
2020-09-16 19:59:49 +0900

    [refactor] "지인 정보 관리 시스템 만들기 프로젝트 완료"

--- src/test/java/com/fastcampus/javaallinone/project3/mycontact/controller/PersonControllerTest.java
+++ src/test/java/com/fastcampus/javaallinone/project3/mycontact/controller/PersonControllerTest.java
@@ -51,6 +51,20 @@ class PersonControllerTest {
     }
 
     @Test
+    void getAll() throws Exception{
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/api/person")
+                    .param("page", "1")
+                    .param("size", "2"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.totalPages").value(3))
+                .andExpect(jsonPath("$.totalElements").value(6))
+                .andExpect(jsonPath("$.numberOfElements").value(2))
+                .andExpect(jsonPath("$.content.[0].name").value("dosang"))
+                .andExpect(jsonPath("$.content.[1].name").value("backjean"));
+    }
+
+    @Test
     void getPerson() throws Exception {
         mockMvc.perform(
                 MockMvcRequestBuilders.get("/api/person/1"))
@@ -94,10 +108,39 @@ class PersonControllerTest {
 
         mockMvc.perform(
                 MockMvcRequestBuilders.post("/api/person")
-                .contentType(MediaType.APPLICATION_JSON_UTF8)
-                .content(toJsonString(dto)))
-                .andExpect(jsonPath("$.code").value(500))
-                .andExpect(jsonPath("$.message").value("알 수 없는 서버 오류가 발생하였습니다."));
+                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .content(toJsonString(dto)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value(400))
+                .andExpect(jsonPath("$.message").value("이름은 필수값입니다"));
+    }
+
+    @Test
+    void postPersonIfNameIsEmpty() throws Exception {
+        PersonDto dto = new PersonDto();
+        dto.setName("");
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/api/person")
+                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .content(toJsonString(dto)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value(400))
+                .andExpect(jsonPath("$.message").value("이름은 필수값입니다"));
+    }
+
+    @Test
+    void postPersonIfNameIsBlankString() throws Exception {
+        PersonDto dto = new PersonDto();
+        dto.setName(" ");
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/api/person")
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(toJsonString(dto)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value(400))
+                .andExpect(jsonPath("$.message").value("이름은 필수값입니다"));
     }
 
     @Test
